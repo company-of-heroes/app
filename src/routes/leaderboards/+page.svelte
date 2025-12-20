e449ca52ba20b179939506d446e0137e3f854335
codeit-ninja <richard@codeit.ninja>
2025-12-20 02:47:58 +0100

    Use the table component for the leaderboards

--- src/routes/leaderboards/+page.svelte
+++ src/routes/leaderboards/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Snapshot } from '@sveltejs/kit';
 	import * as Form from '$lib/components/ui/form';
+	import * as Table from '$lib/components/ui/table';
 	import { cn, getRankImageByLeaderboardId } from '$lib/utils';
 	import { relic } from '$lib/relic';
 	import { resource, useDebounce } from 'runed';
@@ -89,69 +90,70 @@
 </form>
 
 <div class={cn('flex grow flex-col')}>
-	<div
-		class={cn(
-			'border-secondary-600 bg-secondary-700 text-secondary-300 flex items-center gap-1 rounded-md border',
-			'text-secondary-100 [&>span]:flex [&>span]:h-10 [&>span]:items-center [&>span]:px-4 [&>span]:font-semibold'
-		)}
-	>
-		<span class="flex w-12 justify-center">#</span>
-		<span class="flex w-24">Rank</span>
-		<span class="grow">Player</span>
-		<span class="flex w-28 justify-center">Wins</span>
-		<span class="flex w-28 justify-center">Losses</span>
-		<span class="flex w-28 justify-center">Streak</span>
-		<span class="flex w-28 justify-center">Ratio</span>
-	</div>
-	{#if statsResource.loading}
-		{#each Array(5)}
-			<div
-				class={cn(
-					'flex items-center gap-1 rounded-md',
-					'[&>span]:my-2 [&>span]:flex [&>span]:h-3 [&>span]:items-center [&>span]:rounded-full [&>span]:bg-gray-700 [&>span]:px-4'
-				)}
-			>
-				<span class="flex w-12 animate-pulse justify-center"></span>
-				<span class="flex w-24 animate-pulse"></span>
-				<span class="grow animate-pulse"></span>
-				<span class="flex w-28 animate-pulse justify-center"></span>
-				<span class="flex w-28 animate-pulse justify-center"></span>
-				<span class="flex w-28 animate-pulse justify-center"></span>
-				<span class="flex w-28 animate-pulse justify-center"></span>
-			</div>
-		{/each}
-	{:else}
-		{#each filteredStats as stat}
-			<a
-				class={cn(
-					'flex items-center gap-1 rounded-md border-b border-gray-700 odd:bg-gray-800',
-					'[&>span]:flex [&>span]:h-10 [&>span]:items-center [&>span]:px-4',
-					'hover:bg-gray-700'
-				)}
-				href={`/leaderboards/profile/${stat.profile.profile_id}`}
-			>
-				<span class="flex w-12 justify-center">{stat.rank}</span>
-				<span class="relative flex w-24 items-center gap-4">
-					{#await getRankImageByLeaderboardId(stat.leaderboard_id, stat.ranklevel) then rankImage}
-						<img src={rankImage} alt={`${stat.ranklevel}`} class="relative w-6" />
-					{/await}
-					<span class="relative font-medium text-white">{stat.ranklevel}</span>
-				</span>
-				<span class="text-primary-50 grow">{stat.profile?.alias}</span>
-				<span class="flex w-28 justify-center font-medium text-green-200">{stat.wins}</span>
-				<span class="flex w-28 justify-center font-medium text-red-200">{stat.losses}</span>
-				<span
-					class={cn(
-						'flex w-28 justify-center',
-						stat.streak < 0 ? 'text-red-300' : 'text-green-300'
-					)}
-				>
-					{stat.streak}
-				</span>
-				<span class={cn('flex w-28 justify-center', getRatioColor(stat.wins, stat.losses))}>
-					{stat.losses > 0 ? (stat.wins / stat.losses).toFixed(2) : stat.wins > 0 ? '∞' : '0.00'}
-				</span>
-			</a>
-		{/each}
-	{/if}
+	<Table.Table>
+		<Table.THead>
+			<Table.TH width="1/24">#</Table.TH>
+			<Table.TH width="2/24">Rank</Table.TH>
+			<Table.TH width="13/24">Alias</Table.TH>
+			<Table.TH width="2/24" class="text-center">Wins</Table.TH>
+			<Table.TH width="2/24" class="text-center">Losses</Table.TH>
+			<Table.TH width="2/24">Streak</Table.TH>
+			<Table.TH width="2/24">Ratio</Table.TH>
+		</Table.THead>
+		{#if statsResource.loading}
+			{#each Array(5)}
+				<Table.TR>
+					<Table.TD width="1/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="2/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="13/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="2/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="2/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="2/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+					<Table.TD width="2/24" class="animate-pulse">
+						<div class="h-3 w-full rounded-full bg-gray-700"></div>
+					</Table.TD>
+				</Table.TR>
+			{/each}
+		{:else if statsResource.current}
+			{#each filteredStats as stat}
+				<Table.TR href={`/leaderboards/profile/${stat.profile.profile_id}`}>
+					<Table.TD width="1/24" class="text-center font-semibold">{stat.rank}</Table.TD>
+					<Table.TD width="2/24" class="relative flex items-center gap-4">
+						{#await getRankImageByLeaderboardId(stat.leaderboard_id, stat.ranklevel) then rankImage}
+							<img src={rankImage} alt={`${stat.ranklevel}`} class="relative w-6" />
+						{/await}
+						<span class="relative font-medium text-white">{stat.ranklevel}</span>
+					</Table.TD>
+					<Table.TD width="13/24" class="truncate">{stat.profile?.alias}</Table.TD>
+					<Table.TD width="2/24" class="text-center font-medium text-green-200">
+						{stat.wins}
+					</Table.TD>
+					<Table.TD width="2/24" class="text-center font-medium text-red-200">
+						{stat.losses}
+					</Table.TD>
+					<Table.TD
+						width="2/24"
+						class={cn('text-center', stat.streak < 0 ? 'text-red-300' : 'text-green-300')}
+					>
+						{stat.streak}
+					</Table.TD>
+					<Table.TD width="2/24" class={cn('text-center', getRatioColor(stat.wins, stat.losses))}>
+						{stat.losses > 0 ? (stat.wins / stat.losses).toFixed(2) : stat.wins > 0 ? '∞' : '0.00'}
+					</Table.TD>
+				</Table.TR>
+			{/each}
+		{/if}
+	</Table.Table>
 </div>
