bf8569c8f71207a9f9b8f767d39eab2501d07305
codeit-ninja <richard@codeit.ninja>
2026-01-05 20:58:23 +0100

    Fix typo

--- src/routes/(loaded)/replays/[replayId]/+page.svelte
+++ src/routes/(loaded)/replays/[replayId]/+page.svelte
@@ -18,11 +18,11 @@
 	<Replay.Root file={query.current} class="flex grow flex-col gap-4">
 		<Replay.Title class="mb-4" />
 		<Replay.Details class="mb-4" />
-		<Tabs.Root value="timeframe">
+		<Tabs.Root value="overview">
 			<Tabs.List>
 				<Tabs.Trigger value="overview">Overview</Tabs.Trigger>
 				<Tabs.Trigger value="chat">Chat</Tabs.Trigger>
-				<Tabs.Trigger value="timeframe">Timeframe</Tabs.Trigger>
+				<Tabs.Trigger value="timeline">Timeline</Tabs.Trigger>
 			</Tabs.List>
 			<Tabs.Content value="overview" class="flex grow flex-col gap-4">
 				<Replay.Players />
@@ -30,7 +30,7 @@
 			<Tabs.Content value="chat" class="flex grow flex-col gap-4">
 				<Replay.Chat class="grow" />
 			</Tabs.Content>
-			<Tabs.Content value="timeframe" class="flex grow flex-col gap-4">
+			<Tabs.Content value="timeline" class="flex grow flex-col gap-4">
 				<Replay.Actions />
 			</Tabs.Content>
 		</Tabs.Root>
