49c6ca90ab424622a9738e46ff99a77e3f8d8372
Richard Mauritz <richard@codeit.ninja>
2026-03-06 16:58:51 +0100

    Optimize aggregation queries with indexes

--- pocketbase/pb_migrations/1769449900_added_aggregation_indexes.js
+++ pocketbase/pb_migrations/1769449900_added_aggregation_indexes.js
@@ -5,13 +5,17 @@ migrate(
 		lobbies.indexes = [
 			...lobbies.indexes,
 			'CREATE INDEX `idx_lobbies_user_map_agg` ON `lobbies` (`user`, `map`)',
+			'CREATE INDEX `idx_lobbies_user_players_agg` ON `lobbies` (`user`, `players`)',
+			'CREATE INDEX `idx_lobbies_players_agg` ON `lobbies` (`players`)',
 			'CREATE INDEX `idx_lobbies_map_agg` ON `lobbies` (`map`)'
 		];
 
 		const replays = app.findCollectionByNameOrId('pbc_3644265509');
 		replays.indexes = [
 			...replays.indexes,
-			"CREATE INDEX `idx_replays_createdBy_mapName_agg` ON `replays` (`createdBy`, `mapName`) WHERE `createdBy` != ''"
+			"CREATE INDEX `idx_replays_createdBy_mapName_agg` ON `replays` (`createdBy`, `mapName`) WHERE `createdBy` != ''",
+			"CREATE INDEX `idx_replays_createdBy_players_agg` ON `replays` (`createdBy`, `players`) WHERE `createdBy` != ''",
+			'CREATE INDEX `idx_replays_players_agg` ON `replays` (`players`)'
 		];
 
 		app.save(lobbies);
@@ -21,12 +25,18 @@ migrate(
 		const lobbies = app.findCollectionByNameOrId('pbc_1574334436');
 		lobbies.indexes = lobbies.indexes.filter(
 			(index) =>
-				!index.includes('idx_lobbies_user_map_agg') && !index.includes('idx_lobbies_map_agg')
+				!index.includes('idx_lobbies_user_map_agg') &&
+				!index.includes('idx_lobbies_user_players_agg') &&
+				!index.includes('idx_lobbies_players_agg') &&
+				!index.includes('idx_lobbies_map_agg')
 		);
 
 		const replays = app.findCollectionByNameOrId('pbc_3644265509');
 		replays.indexes = replays.indexes.filter(
-			(index) => !index.includes('idx_replays_createdBy_mapName_agg')
+			(index) =>
+				!index.includes('idx_replays_createdBy_mapName_agg') &&
+				!index.includes('idx_replays_createdBy_players_agg') &&
+				!index.includes('idx_replays_players_agg')
 		);
 
 		app.save(lobbies);
