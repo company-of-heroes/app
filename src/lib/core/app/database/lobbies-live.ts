c65397321fe805419c99d1b2429ec311a062d2c8
Richard Mauritz <richard@codeit.ninja>
2026-04-16 03:06:17 +0200

    Add live lobbies

--- src/lib/core/app/database/lobbies-live.ts
+++ src/lib/core/app/database/lobbies-live.ts
@@ -1,31 +1,37 @@
-import type { Match } from "../context/lobby.svelte";
-import { fetch } from "@tauri-apps/plugin-http";
+import type { Match } from '../context/lobby.svelte';
+import { fetch } from '@tauri-apps/plugin-http';
 import { exp, pocketbase } from '$core/pocketbase';
-import { auth } from "../features/auth";
+import { auth } from '../features/auth';
 
 export class LobbiesLive {
-    setLobby(match: Match) {
-        pocketbase.collection('lobbies_live')
-            .getFirstListItem('', { fetch })
-            .then(existingLobby => {
-                pocketbase.collection('lobbies_live').update(existingLobby.id, {
-                    user: auth.userId,
-                    isRanked: match.isRanked,
-                    sessionId: match.sessionId,
-                    map: match.map,
-                    players: match.players,
-                }, { fetch })
-                .then(lobby => console.log(lobby))
-            })
-            .catch(() => {
-                pocketbase.collection('lobbies_live').create({
-                    user: auth.userId,
-                    isRanked: match.isRanked,
-                    sessionId: match.sessionId,
-                    map: match.map,
-                    players: match.players,
-                }, { fetch })
-                .then(lobby => console.log(lobby)) 
-            })
-    }
+	setLobby(match: Match) {
+		return pocketbase
+			.collection('lobbies_live')
+			.getFirstListItem('', { fetch })
+			.then((existingLobby) => {
+				return pocketbase.collection('lobbies_live').update(
+					existingLobby.id,
+					{
+						user: auth.userId,
+						isRanked: match.isRanked,
+						sessionId: match.sessionId,
+						map: match.map,
+						players: match.players
+					},
+					{ fetch }
+				);
+			})
+			.catch(() => {
+				return pocketbase.collection('lobbies_live').create(
+					{
+						user: auth.userId,
+						isRanked: match.isRanked,
+						sessionId: match.sessionId,
+						map: match.map,
+						players: match.players
+					},
+					{ fetch }
+				);
+			});
+	}
 }
